f6c7bc62290a46a9f8b3cc9ee8de82d35f24f92d
masatotakahashi31 <mas-takahashi@autobacs.com>
2026-09-09 02:58:14 +0000

    [update]コードの整理
    コードが読みにくかったので整理しました

--- src/app/fridge/actions.ts
+++ src/app/fridge/actions.ts
@@ -8,24 +8,15 @@ import {redirect} from 'next/navigation'
 
 //フォームから送られてきたデータ（formData）を受け取る非同期関数を定義し、外部から呼び出せるよう公開（export
 export async function addStock(formData: FormData){
+    
+    // ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+    // ① 食材（Food）とカテゴリに関するデータの準備・登録
+    // ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+    
     // フォームから送られたカテゴリIDを数値にして取得
     const categoryId = Number(formData.get('categoryId'))
     // 食材名を文字列として取得
     const foodName = formData.get('foodName') as string
-    // 数量を数値にして取得
-    const quantity = Number(formData.get('quantity'))
-
-    // フォームから送られた消費期限のデータを、まずは文字列（例: "2026-09-08" や 空文字 ""）として変数に保存
-    const expirationDateString = formData.get('expirationDate') as string
-    // 「もし入力されていれば、文字列を日付データ（Date）に変換する。もし空っぽなら null にする」という条件分岐を行い、結果を変数に保存
-    const expirationDate = expirationDateString ? new Date(expirationDateString) : null
-
-    // StockForm.tsx）の <input name="memo" /> で入力された文字を引っ張ってきています
-    const memoString = formData.get('memo') as string
-    // 「条件演算子（三項演算子）」という書き方
-    // もし memoString に何か文字が入っていたら、そのままその文字を使う
-    // 空っぽ（未入力）だったら、強制的に null（データなし）に変換
-    const memo = memoString ? memoString : null
 
     // 食品マスタ）テーブルの中から、条件に合う最初の1件を探して変数 food に入れます。後で中身を書き換える可能性があるため、const ではなく let で宣言
     let food = await prisma.food.findFirst({
@@ -60,6 +51,26 @@ export async function addStock(formData: FormData){
     // 見つからなかった場合の分岐を閉じ
     }
 
+
+    // ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+    // ② 在庫（Stock）に関するデータの準備・登録
+    // ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+
+    // 数量を数値にして取得
+    const quantity = Number(formData.get('quantity'))
+
+    // フォームから送られた消費期限のデータを、まずは文字列（例: "2026-09-08" や 空文字 ""）として変数に保存
+    const expirationDateString = formData.get('expirationDate') as string
+    // 「もし入力されていれば、文字列を日付データ（Date）に変換する。もし空っぽなら null にする」という条件分岐を行い、結果を変数に保存
+    const expirationDate = expirationDateString ? new Date(expirationDateString) : null
+
+    // StockForm.tsx）の <input name="memo" /> で入力された文字を引っ張ってきています
+    const memoString = formData.get('memo') as string
+    // 「条件演算子（三項演算子）」という書き方
+    // もし memoString に何か文字が入っていたら、そのままその文字を使う
+    // 空っぽ（未入力）だったら、強制的に null（データなし）に変換
+    const memo = memoString ? memoString : null
+
     // 最後に、stock（在庫）テーブルへの登録処理を開始
     // 左側の stockQuantity は、データベースで決めた列の名前
     // 右側の quantity は、画面から受け取って作った変数の名前
@@ -80,6 +91,10 @@ export async function addStock(formData: FormData){
     // すべて完了したら冷蔵庫一覧画面へ移動
     redirect('/fridge')
 }
+
+// ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+// ③ 削除機能
+// ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
 
 // この関数を呼び出すときに、「必ず数字（number）のIDを一つ渡し
 export async function deleteStockById(id: number){
